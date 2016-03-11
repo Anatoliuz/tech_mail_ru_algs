5787b95c1e25780ec788f45b923def6d37de153a
Anatoliuz <afilinovs@gmail.com>
2016-03-12 02:33:15 +0300

    Program structut of p3 fixed

--- p3/main.cpp
+++ p3/main.cpp
@@ -15,39 +15,12 @@
  */
 
 //Решение
-
 /*
- i, j обозначим pos
- n -> sizeOfA
- m -> sizeOfB
- powOfTwoIndex - текущая степень двойки, чтобы вызывать бинарный поиск более эффективно.
- 
- Для каждого элемента B[posB] будем проверять выполнимость некоторых условий
- относительно элементов из А.
- В зависимости от условия, выполняем определенные шаги.
- Также имеется немного модифицированный бинарный поиск, который
- в случае не нахождения элемента A[index] == B[jndex] в заданном интервале, воз-
- вращает индекс ближайшего большего элемента.
- Условия и шаги:
- 1) Если B[posB] == A[0] мы нашли индекс для i-го B, выведем 0 в поток.
- Приступаем к работе со следующим элементом из В.
- 2) Если B[posB] > A[sizeOfA - 1] выведем в поток sizeOfA, приступаем к следующему
- элементу из В.
- Далее posA будет степенью двойки
- 3) Если элементы равны, выводим posA, приступаем к следующему элементу из B.
- 4) Если A[posA] > B[posB]
-     4.1. Если двойка в степени powOfTwoIndex == 1, мы смотрим нулевой элемент из А(т.е.
-      делаем первый проход по второму вложенному циклу), вызываем бинарный поиск в интервале
-      от 0 до 2^0 = 1; бинарный поиск находит индекс элемента из А либо равного элементу из
-      B, либо ближайшего большего числа.
-      Выводим индекс, который вернет бинарный поиск, приступаем к следующему элементу из B.
-     4.2. Если проход по второму циклу не первый,мы вызываем бинарный поиск в интервале от 2^x-1 до 2^x
-      Тем самым удовлетворяя сложности О(logk) 
-      Выводим индекс, который вернет бинарный поиск, приступаем к следующему элементу из B.
-     4.3. Если мы не нашли элемент в интервале 2^x до 2^x-1, смещаем отрезок поиска, но если двойка
-       в новой степени больше sizeOfA - 1 интервал будет иметь вид от 2^x(i)-1 до sizeOfA - 1
+	Функция Output()  считывает данные пользователя и в цикле для каждого
+	элемента из В вызывает функцию LogSearch, которая за логарифмическое время 
+	возвращает индекс элемента из массива А, большего либо равного данному элементу.
+*/
 
- */
 #include <iostream>
 #include <math.h>
 
@@ -69,71 +42,60 @@ int BinSearch(int* array, int searchingElement, int left, int right){
     }
 }
 
-void FindIndexes(){
+int LogSearch(int* Array, int sizeOfA, int findNum){
+    int powOfTwoIndex = 1; 
+    if (findNum == Array[0]) {
+        return 0;
+    }
+    if (Array[sizeOfA - 1] < findNum ) {
+        return sizeOfA;
+    }
+    for(int posA = 1; posA < sizeOfA; ) {
+        if (findNum == Array[posA]) {
+            return posA;
+        }
+        else if (Array[posA] > findNum) {
+            if (pow(2, powOfTwoIndex - 1) == 1) { //если posA = 0
+                return BinSearch(Array, findNum, 0, 1);//ищем в массиве А в интервале
+                									// от 0 до 1 
+            }
+            else{ //иначе в интервале от 2^k-1 до 2^k
+                return BinSearch(Array, findNum, pow(2, powOfTwoIndex - 1), posA);
+            }     
+        }
+        else {
+            posA = pow(2, ++powOfTwoIndex);//увеличиваем верхнюю границу поиска
+            if (posA > sizeOfA - 1) {
+                posA = sizeOfA - 1; //проверка на не выход за границу массива
+            }
+        }
+    }
+    return -1; //ошибка
+}
+
+
+void Output(){
     int sizeOfA = 0;
     int sizeOfB = 0;
     std::cin >> sizeOfA >> sizeOfB;
-    std::cout << std::endl;
-    int A[sizeOfA];
-    int B[sizeOfB];
-    
+    int* A = new int[sizeOfA];
+    int* B = new int[sizeOfB];
     for (int pos = 0; pos < sizeOfA; ++pos) {
         std::cin >> A[pos];
     }
-    std::cout << std::endl;
-    
     for (int pos = 0; pos < sizeOfB; ++pos) {
         std::cin >> B[pos];
     }
-    int powOfTwoIndex = 1;
-    for (int posB = 0; posB < sizeOfB; ++posB) {
-        //1 шаг
-        if (B[posB] == A[0]) {
-            std::cout << 0 << " ";   
-            continue;
-        }
-        // 2 шаг
-        if (A[sizeOfA - 1] < B[posB] ) {
-            std::cout<< sizeOfA << " " ; 
-            continue;
-        }
-        powOfTwoIndex = 1;
-        for(int posA = 1; posA < sizeOfA; ) {
-             //3 шаг
-            if (B[posB] == A[posA]) {    
-                std::cout<< posA << " ";
-                posA = sizeOfA + 1;
-                continue;
-            }
-            //4 шаг
-            else if (A[posA] > B[posB]) { 
-                //4.1 шаг
-                if (pow(2, powOfTwoIndex - 1) == 1) {
-                    int k = BinSearch(A, B[posB], 0, 1);
-                    std::cout << k << " ";
-                    posA = sizeOfA + 1;
-                }
-                //4.2 шаг
-                else{
-                    int k = BinSearch(A, B[posB], pow(2, powOfTwoIndex - 1), posA);
-                    std::cout << k << " ";
-                    posA = sizeOfA + 1;
-                }
-                
-            }
-            //4.3 шаг
-            else {
-                posA = pow(2, ++powOfTwoIndex);
-                if (posA > sizeOfA - 1) {
-                    posA = sizeOfA - 1; //проверка на не выход за границу массива
-                }
-            }
-        }
+    for (int posB = 0; posB < sizeOfB; ++posB)
+    {
+        std::cout << LogSearch(A, sizeOfA, B[posB]) <<" ";
     }
+    delete[] A;
+    delete[] B;
 }
 int main(int argc, const char * argv[])
 {
-    FindIndexes();
+    Output();
     return 0;
 }
 
