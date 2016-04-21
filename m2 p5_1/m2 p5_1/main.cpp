9fe683feae77a47a2edad246ec70598247fa5cdf
Anatoliuz <afilinovs@gmail.com>
2016-04-21 16:39:16 +0300

    38 error

--- m2 p5_1/m2 p5_1/main.cpp
+++ m2 p5_1/m2 p5_1/main.cpp
@@ -1,69 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstring>
 
 using namespace std;
-bool less(const int &lhs, const int& rhs){
-    return lhs < rhs;
-}
+
 int charAt(string s, int d)
 {
-    
     if (d < s.size())
         return s[d];
     else
-        return 0;
+        return -1;
 }
 
-class MSD
+
+void msd_sort(vector<string>& v, vector<string>& aux, int pos, int lo, int hi)
 {
-private:
-    int capacity = 256;
-    vector<string> aux;       // auxiliary array for distribution
+    if ( hi <= lo ) return;
     
-public:
+    vector<int> count;
+    count.resize(256);
+    
+    for ( auto i = lo; i <= hi; ++i )
+        ++count[charAt(v[i], pos) + 2];
     
     
-    void sort(vector<string>& a)
+    for (auto i = 1; i < count.size(); ++i)
+        count[i] += count[i-1];
+    
+    for ( auto i = lo; i <= hi; ++i )
+        aux[count[charAt(v[i], pos)+1]++] = v[i];
+    
+    for ( int i = lo; i <= hi; ++i )
+        v[i] = aux[i - lo];
+    
+    for ( int r = 0; r < count.size(); ++r )
     {
-        aux.resize(a.size());
-        msd_sort(a, 0, a.size() - 1, 0);
+        msd_sort(v, aux, pos+1, lo + count[r], lo + count[r+1] - 1);
     }
-    
-    void msd_sort(vector<string>& a, int lo, int hi, int d)
-    {  // Sort from a[lo] to a[hi], starting at the dth character.
-        // int* count = new int[capacity+1]();
-        int count[capacity + 1];
-        for (int i = 0; i <= capacity; ++i) {
-            count[i] = 0;
-        }
-        if(hi <= lo) return;
-        
-        for (size_t i = lo; i <= hi; ++i)
-            ++count[charAt(a[i], d) + 1];
-        for (size_t i = 1; i < capacity; ++i)
-            count[i] += count[i - 1];
-        for (size_t i = lo; i <= hi; ++i)
-            aux[count[charAt(a[i], d)]++] = a[i];
-        for (size_t i = lo; i <= hi; ++i)
-            a[i] = aux[i];
-        for (int r = 0; r < capacity-1; r++)
-            msd_sort(a, lo + count[r], lo + count[r+1] - 1, d+1);
-    }
-    
-};
+}
 
-
+void sort(vector<string>& v)
+{
+    vector<string> aux(v.size());
+    msd_sort(v, aux, 0, 0, v.size()-1);
+}
 
 
 int main() {
     int c;
     string str;
     vector<string> str_array;
-    MSD msdclass;
-    
-    
     while((c = getc(stdin)) != EOF){
         if(c == '\n' && !str.empty()){
             str_array.push_back(str);
@@ -75,13 +61,9 @@ int main() {
         str_array.push_back(str);
         str.clear();
     }
-    
-    msdclass.sort(str_array);
-    
-    cout << "After sort" << endl;
+    sort(str_array);
     for (vector<string>::iterator it = str_array.begin(); it < str_array.end(); ++it) {
         cout << *it << endl;
     }
-    // MSD_sort(str_array);
     return 0;
 }
