7f31ef11a2db5ed1a20075601c3621fb18dd076f
Anatoliuz <afilinovs@gmail.com>
2016-04-21 19:52:09 +0300

    final m2 p5

--- m2 p5_1/m2 p5_1/main.cpp
+++ m2 p5_1/m2 p5_1/main.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#define capacity 257
 using namespace std;
 
-int charAt(string s, int d)
+int charAt(string word, int pos_int_word)
 {
-    if (d < s.size())
-        return s[d];
+    if (pos_int_word < word.size())
+        return word[pos_int_word];
     else
-        return -1;
+        return 0;
 }
 
 
-void msd_sort(vector<string>& v, vector<string>& aux, int pos, int lo, int hi)
+void msd_sort(vector<string>& str_array, int lo, int hi, vector<string>& aux, int pos_in_word )
 {
     if ( hi <= lo ) return;
     
     vector<int> count;
-    count.resize(256);
+    count.resize(capacity);
+   
+    for ( int i = lo; i <= hi; ++i )
+        ++count[charAt(str_array[i], pos_in_word) + 1];
     
-    for ( auto i = lo; i <= hi; ++i )
-        ++count[charAt(v[i], pos) + 2];
-    
-    
-    for (auto i = 1; i < count.size(); ++i)
+    for (int i = 1; i < count.size(); ++i)
         count[i] += count[i-1];
     
-    for ( auto i = lo; i <= hi; ++i )
-        aux[count[charAt(v[i], pos)+1]++] = v[i];
+    for ( int i = lo; i <= hi; ++i )
+        aux[count[charAt(str_array[i], pos_in_word) ]++] = str_array[i];
     
     for ( int i = lo; i <= hi; ++i )
-        v[i] = aux[i - lo];
+        str_array[i] = aux[i - lo];
     
     for ( int r = 0; r < count.size(); ++r )
     {
-        msd_sort(v, aux, pos+1, lo + count[r], lo + count[r+1] - 1);
+        msd_sort(str_array,  lo + count[r], lo + count[r+1] - 1, aux,  pos_in_word+1);
     }
 }
 
 void sort(vector<string>& v)
 {
     vector<string> aux(v.size());
-    msd_sort(v, aux, 0, 0, v.size()-1);
+    msd_sort(v, 0, v.size()-1, aux, 0);
 }
 
 
@@ -61,6 +60,7 @@ int main() {
         str_array.push_back(str);
         str.clear();
     }
+    
     sort(str_array);
     for (vector<string>::iterator it = str_array.begin(); it < str_array.end(); ++it) {
         cout << *it << endl;
